1fca142d61985f99cd553b7efd7b2d2396fb7c3c
Sandy <machadogitt@gmail.com>
2025-03-24 17:02:25 -0300

    Postagens - Lista e Card

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { AuthProvider } from './contexts/AuthContext'
 import ListaTemas from './components/temas/listatemas/ListaTemas'
 import FormTema from './components/temas/formtema/FormTema'
 import DeletarTema from './components/temas/deletartema/DeletarTema'
+import ListaPostagens from './components/postagens/listapostagens/ListaPostagens'
 
 function App() {
   return (
@@ -24,8 +25,9 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/temas" element={<ListaTemas />} />
               <Route path="/cadastrartema" element={<FormTema />} />
-<Route path="/editartema/:id" element={<FormTema />} />
-<Route path="/deletartema/:id" element={<DeletarTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
+               <Route path="/deletartema/:id" element={<DeletarTema />} />
+               <Route path="/postagens" element={<ListaPostagens />} />
 
             </Routes>
           </div>
