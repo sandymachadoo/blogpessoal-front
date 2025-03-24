0e2f2df37a16ba2d436d239cf89cb8c7cd7eca21
Sandy <machadogitt@gmail.com>
2025-03-24 17:23:49 -0300

    Finalizando postagem e criação de perfil

--- src/components/postagens/cardpostagens/CardPostagens.tsx
+++ src/components/postagens/cardpostagens/CardPostagens.tsx
@@ -31,16 +31,16 @@ function CardPostagem({ postagem }: CardPostagensProps) {
                 </div>
             </div>
             <div className="flex">
-                <Link to='' 
-                    className='w-full text-white bg-indigo-400 
-                    hover:bg-indigo-800 flex items-center justify-center py-2'>
-                    <button>Editar</button>
-                </Link>
-                <Link to='' 
-                    className='text-white bg-red-400 
-                    hover:bg-red-700 w-full flex items-center justify-center'>
-                    <button>Deletar</button>
-                </Link>
+            <Link to={`/editarpostagem/${postagem.id}`}
+	       className='w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800 
+           flex items-center justify-center py-2'>
+	       <button>Editar</button>
+           </Link>
+           <Link to={`/deletarpostagem/${postagem.id}`} 
+	      className='text-white bg-red-400 
+	      hover:bg-red-700 w-full flex items-center justify-center'>
+	     <button>Deletar</button>
+         </Link>
             </div>
         </div>
     )
